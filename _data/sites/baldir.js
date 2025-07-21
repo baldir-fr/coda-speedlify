72ad996eef347a439c7ef936887a2af949f3f0aa
Marc Bouvier <m.bouvier.dev@gmail.com>
2025-07-21 23:11:31 +0200

    split baldir scans (1 run per day) and coda b1 (1 run per 15 min)

--- _data/sites/baldir.js
+++ _data/sites/baldir.js
@@ -3,7 +3,7 @@ module.exports = {
 	description: "Sites statiques Coda 2025 B1",
 	options: {
 		runs: 1,
-		frequency: 15, // 15 minutes
+		frequency: 60 * 23, // 24 hours
 		// Use "run" if the sites don’t share assets on the same origin
 		//           and we can reset chrome with each run instead of
 		//           each site in every run (it’s faster)
@@ -11,5 +11,9 @@ module.exports = {
 		freshChrome: "run"
 	},
 	urls: [
+		"https://baldir-fr.github.io/ma-page/",
+		"https://bc.baldir.fr/",
+		"https://u.baldir.fr/",
+		"https://baldir-fr.github.io/coda-speedlify/ssg-coda-2025-b1/"
 	]
 };
